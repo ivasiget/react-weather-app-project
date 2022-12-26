1f60c278af5b3ff130b3f36282b7e19748040481
ivasiget <iva.siget@gmail.com>
2022-12-26 08:10:29 +0100

    cleaned the code, renamed to weather app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Weather app</h1>
+
+      <footer>
+        <a href="" target_blank>
+          {" "}
+          Open-sourced code
+        </a>{" "}
+        on Github by Iva Siget.
+      </footer>
     </div>
   );
 }
