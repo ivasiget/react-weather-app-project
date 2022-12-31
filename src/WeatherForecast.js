1984e2087e2fce965e2f8a9a45408ae2369db851
ivasiget <iva.siget@gmail.com>
2022-12-31 12:00:24 +0100

    imported useEffect to update forecast data on change

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./WeatherForecast.css";
 import WeatherForecastDay from "./WeatherForecastDay";
 
@@ -7,6 +7,9 @@ export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
 
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
   function handeResponse(response) {
     console.log(response.data);
     setLoaded(true);
